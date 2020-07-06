ff421e9dc5b8ccb11ee32d6dc88e08562c5c746e
MozhaevIL <ptitzzz@gmail.com>
2020-07-06 20:00:44 +0300

    task completed

--- src/js/__tests__/Character.test.js
+++ src/js/__tests__/Character.test.js
@@ -24,3 +24,77 @@ test('must throw an error when invalid type is given', () => {
     errName();
   }).toThrow('Недопустимый тип персонажа');
 });
+
+test('must commit levelUp method correctly', () => {
+  const expected = {
+    name: 'hero',
+    type: 'Bowman',
+    health: 100,
+    level: 2,
+    attack: 30,
+    defence: 30,
+  };
+
+  const recieved = new Character('hero', 'Bowman');
+  recieved.health = 10;
+  recieved.attack = 25;
+  recieved.defence = 25;
+  recieved.levelUp();
+
+  expect(recieved).toEqual(expected);
+});
+
+test('must throw error on leveling up dead character', () => {
+  expect(() => {
+    const deadBowman = new Character('hero', 'Bowman');
+    deadBowman.health = 0;
+    deadBowman.levelUp();
+  }).toThrow('Персонаж мертв');
+});
+
+test('must calculate taken damage correctly if character is alive', () => {
+  const expected = {
+    name: 'hero',
+    type: 'Bowman',
+    health: 85,
+    level: 1,
+    attack: 25,
+    defence: 25,
+  };
+
+  const recieved = new Character('hero', 'Bowman');
+  recieved.health = 100;
+  recieved.attack = 25;
+  recieved.defence = 25;
+  recieved.damage(20);
+
+  expect(recieved).toEqual(expected);
+});
+
+test('must calculate taken damage correctly if character is dying', () => {
+  const expected = {
+    name: 'hero',
+    type: 'Bowman',
+    health: 0,
+    level: 1,
+    attack: 25,
+    defence: 25,
+  };
+
+  const recieved = new Character('hero', 'Bowman');
+  recieved.health = 1;
+  recieved.attack = 25;
+  recieved.defence = 25;
+  recieved.damage(20);
+
+  expect(recieved).toEqual(expected);
+});
+
+test('must throw an error on damage to dead character', () => {
+  expect(() => {
+    const deadBowman = new Character('hero', 'Bowman');
+    deadBowman.health = 0;
+    deadBowman.defence = 25;
+    deadBowman.damage(10);
+  }).toThrow('Персонаж уже мертв');
+});
